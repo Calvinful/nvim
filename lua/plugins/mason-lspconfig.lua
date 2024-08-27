46b95406cec007b1cbbc0e46be73afaf2d7fee18
Calvinful <53443776+Calvinful@users.noreply.github.com>
2024-08-27 19:52:04 -0400

    fixed lsp

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,41 +1,57 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  -- Mason and related plugins
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+      -- Configure clangd
+      lspconfig.clangd.setup{
+        cmd = { "clangd", "--background-index" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        -- Add any other settings you want
+      }
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+      -- Set up key bindings for LSP functionality
+      local opts = { noremap=true, silent=true }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"clangd",
-				"tsserver",
-				"glsl_analyzer",
-			},
-		})
+      -- Enable diagnostics
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = true,
+      })
+    end
+  },
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-			},
-		})
-	end,
+  -- Keep your existing treesitter configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {"c", "cpp", "lua", "javascript", "java", "llvm"},
+        highlight = { enable = true },
+      }
+    end,
+  }
 }
