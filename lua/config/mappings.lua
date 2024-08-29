c5a6f8b01e1040cda55a045eb21447a5bbfd442e
Calvinful <53443776+Calvinful@users.noreply.github.com>
2024-08-29 14:45:54 -0400

    Additional keybindings for window control + <leader>+e for nvim-tree

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -13,6 +13,21 @@ vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>Telescope find_files<CR>", { no
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
 --vim.api.nvim_set_keymap('n', '<C-h>', '<Nop>', { noremap = true, silent = true })
 
+
+vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>Telescope help_tags<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+
+--WINDOW RESIZING
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+
+vim.api.nvim_set_keymap('n', '<leader>e', "<cmd>NvimTreeOpen <CR>", { noremap = true, silent = true })
+
+
+
 vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 vim.keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 vim.keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
@@ -31,4 +46,6 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 --Fallback because MacOS sends ctrl+h as crtl-b
 --vim.keymap.set("n", "<c-b>", ":wincmd h<CR>")
+vim.keymap.set("n", "<leader><tab>p", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab with \+tab+p
+vim.keymap.set("n", "<leader><tab>o", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab with \+tab+o
 
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -34,7 +34,9 @@ vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
 	vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 	vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    --	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+   	--vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end) -- Need to rebind from ctrl-h. Ctrl-h is window navigation.
+
 	vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 	vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 	vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
