5ab438f409cb7d17adbb6a59113831c8326a183b
Calvinful <53443776+Calvinful@users.noreply.github.com>
2024-08-29 17:52:37 -0400

    Harpoon fully functional through option-JKL; keys with \a to add, and option-e to open list

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 
+vim.g.mapleader = "\\"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.tabstop = 4        -- Number of spaces that a <Tab> in the file counts for
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -30,16 +30,19 @@ vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
 -- REQUIRED
 	harpoon:setup()
 -- REQUIRED
+   --vim.api.nvim_set_keymap('n', '<leader>a', ':lua print("Test")<CR>', { noremap = true, silent = true })
 
+--vim.g.mapleader = "\\"
+--remove this mapleader lol 
 	vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-	vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+	vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     --	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-   	--vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end) -- Need to rebind from ctrl-h. Ctrl-h is window navigation.
+   	vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end) -- Need to rebind from ctrl-h. Ctrl-h is window navigation.
 
-	vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-	vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-	vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+	vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+	vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+	vim.keymap.set("n", "<M-;>", function() harpoon:list():select(4) end)
 
 	-- Toggle previous & next buffers stored within Harpoon list
 	vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
