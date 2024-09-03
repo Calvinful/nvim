cc1d759c152e059420606914bcdef9939237bd00
Calvinful <53443776+Calvinful@users.noreply.github.com>
2024-09-03 19:53:18 -0400

    adjustments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,27 @@
     build = ":TSUpdate", -- Automatically run :TSUpdate after install
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = {"c", "cpp", "lua", "javascript","java", "llvm"},
+        ensure_installed = {"c",
+            "cpp",
+            "lua",
+            "javascript",
+            "java",
+            "llvm",
+            "bash",
+            "html",
+            "json",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "tsx",
+            "typescript",
+            "vim",
+             "yaml",
+
+
+    },
         highlight = { enable = true },
       }
     end,
