6562fa330c9c99dc3f212d7cca88433ce97a0b01
Calvinful <53443776+Calvinful@users.noreply.github.com>
2024-12-25 03:49:48 -0500

    rust

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,29 +1,7 @@
-return{
-require'lspconfig'.rust_analyzer.setup({
-    on_attach = function(client, bufnr)
-        -- Add your on_attach functionality here
-        vim.lsp.set_log_level("debug")
-    end,
-    settings = {
-        ["rust-analyzer"] = {
-            diagnostics = {
-                enable = true,
-            },
-            cargo = {
-                allFeatures = true, -- Enable all features for cargo
-            },
-            procMacro = {
-                enable = true, -- Enable procedural macro support
-            },
-        },
-    },
-})
+return{{
+  'mrcjkb/rustaceanvim',
+  version = '^5', -- Recommended
+  lazy = false, -- This plugin is already lazy
+},
 
-
-}--        {'rust-lang/rust.vim' },
---    {
---        'mrcjkb/rustaceanvim',
-        --version = '^3',
-    --    ft = { 'rust' },
-
-   -- }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,6 +21,7 @@
             "typescript",
             "vim",
              "yaml",
+             "rust",
 
 
     },
